6b24665a673c441fe23e38a7c527a97d9cc75f2a
Daniel Fosco <danielfosco@gmail.com>
2015-07-03 18:10:09 -0300

    Added `mock` object to supply variables to the system's components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import NavBar from './components/navbar/NavBar';
 
 export default class App extends Component {
   render() {
+    var mock = {
+      isOpen: true,
+      copy: 'Este card não é muito maneiro',
+      title: 'Card Maneiro'
+    };
+
     return (
       <div className="str-content">
         <Card title="Card Maneiro" copy="Este card é muito maneiro"/>
